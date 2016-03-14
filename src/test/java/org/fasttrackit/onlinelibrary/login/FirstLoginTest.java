4c4985527a75d2d18670f9ce185e730fe60eea35
Robert-TK <roberttk86@yahoo.com>
2016-03-14 21:09:03 +0200

    Successful password change test.

--- src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
+++ src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
@@ -1,5 +1,6 @@
 package org.fasttrackit.onlinelibrary.login;
 
+import com.sdl.selenium.web.utils.Utils;
 import org.fasttrackit.util.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
@@ -65,6 +66,35 @@ public class FirstLoginTest extends TestBase {
         assertThat(errorMsg.getText(), is(message));
     }
 
+    @Test
+    public void successChangePassword() {
+        openLoginPage();
+        doLogin("eu@fast.com", "eu.pass");
+        WebElement preferencesButton = driver.findElement(By.xpath("//nav//button"));
+        preferencesButton.click();
+
+        Utils.sleep(300);
+
+        //css selector: $$("#preferences-win input[name=password]")
+
+        WebElement currentPassField = driver.findElement(By.xpath("//div[@id='preferences-win']//input[@name='password']"));
+        currentPassField.sendKeys("eu.pass");
+
+        WebElement newPassField = driver.findElement(By.xpath("//input[@name='newPassword']"));
+        newPassField.sendKeys("eu.pass2");
+
+        WebElement repeatPassField = driver.findElement(By.xpath("//div[@id='preferences-win']//input[@name='newPasswordRepeat']"));
+        repeatPassField.sendKeys("eu.pass2");
+
+        WebElement saveBtn = driver.findElement(By.cssSelector("#preferences-win button.btn-warning"));
+        saveBtn.click();
+
+        WebElement statusElement= driver.findElement(By.cssSelector("#preferences-win .status-msg"));
+        System.out.println(statusElement.getText());
+        assertThat(statusElement.getText(), is("Your password has been successfully changed."));
+
+    }
+
 
     private void openLoginPage() {
         System.out.println("Open Login Page.");
@@ -86,3 +116,4 @@ public class FirstLoginTest extends TestBase {
 
 
 }
+
