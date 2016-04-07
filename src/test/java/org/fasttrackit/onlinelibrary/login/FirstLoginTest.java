911d41b30940a5fc8e1ffc90b67a7c427ace9add
Robert-TK <roberttk86@yahoo.com>
2016-04-07 20:57:53 +0300

    Adding an upload test

--- src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
+++ src/test/java/org/fasttrackit/onlinelibrary/login/FirstLoginTest.java
@@ -1,10 +1,16 @@
 package org.fasttrackit.onlinelibrary.login;
 
+import com.sdl.selenium.bootstrap.button.UploadFile;
+import com.sdl.selenium.bootstrap.form.Form;
 import com.sdl.selenium.web.link.WebLink;
+import com.sdl.selenium.web.utils.PropertiesReader;
+import com.sun.xml.internal.fastinfoset.sax.Properties;
 import org.fasttrackit.example.LoginView;
 import org.fasttrackit.util.TestBase;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static com.sdl.selenium.web.utils.PropertiesReader.RESOURCES_DIRECTORY_PATH;
 
 public class FirstLoginTest extends TestBase {
 
@@ -38,9 +44,23 @@ public class FirstLoginTest extends TestBase {
         loginPage.assertThatErrorIs(errorMsg);
     }
 
+    private Form form = new Form("Form Title");
+    private UploadFile uploadBtn = new UploadFile(form, "TPT Test:");
+
+    @Test
+    public void uploadTest() {
+        openLoginPage();
+        System.out.println(PropertiesReader.RESOURCES_DIRECTORY_PATH);
+        uploadBtn.upload("Select file", new String[]{
+                PropertiesReader.RESOURCES_DIRECTORY_PATH + "\\upload.exe",
+                PropertiesReader.RESOURCES_DIRECTORY_PATH + "\\feature\\login\\login.feature"
+        });
+
+    }
+
     private void openLoginPage() {
         System.out.println("Open Login Page.");
-        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/login.html");
+        driver.get("https://rawgit.com/sdl/Testy/master/src/test/functional/app-demo/bootstrap/index.html");
     }
 
 
